32bd57e402f013ef5acb5305930fbdd8750d4dd4
k.f.kyofan <k.f.kyofan@gmail.com>
2026-02-02 21:00:30 +0000

    chore(v2.2.0): Marking v2.2.0 for implementing real 3D scenes and dependencies to integrate with face-tracking

--- main.tsx
+++ main.tsx
@@ -188,7 +188,7 @@ export default function SpatialTextInput({
         animate={{ opacity: 0.7 }}
         transition={{ delay: 1 }}
       >
-        v2.1.0
+        v2.2.0
       </motion.div>
     </div>
   );
