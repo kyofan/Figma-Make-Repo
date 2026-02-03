85013e0a32d6fa9ba1dec0119e36d34885ba15ab
kyofan <k.f.kyofan@gmail.com>
2026-02-03 06:27:49 -0800

    chore(race-condition): Marking v2.6.0 on fixing speech recognition race condition logic and optimize hand-tracking, add InteractionGuide.md

--- main.tsx
+++ main.tsx
@@ -137,7 +137,7 @@ export default function SpatialTextInput({
         animate={{ opacity: 0.7 }}
         transition={{ delay: 1 }}
       >
-        v2.0.0
+        v2.6.0
       </motion.div>
     </div>
   );
